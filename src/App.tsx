95b0b891b7de79b515014a41d3f4ad059dc54346
Nathan Garrett <nathangdev@gmail.com>
2020-05-26 16:04:44 -0400

    auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Auth from "./components/Auth";
+import Navigations from "./components/Navigations";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum UserRoles {
+  admin = "admin",
+  user = "user",
 }
+const userRoles = {
+  admins: [String(UserRoles.admin)],
+  users: [String(UserRoles.user)],
+  all: [String(UserRoles.admin), String(UserRoles.user)],
+};
+const App: React.FunctionComponent = () => {
+  const [sessionToken, setSessionToken] = React.useState<any>("");
+
+  React.useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken: any) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log("updateToken -> newToken", newToken);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navigations updateToken={updateToken} />
+      </div>
+      <Switch>
+        <Route exact path="/signup">
+          <Auth updateToken={updateToken} />
+        </Route>
+        <Route exact path="/login">
+          <Login updateToken={updateToken} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/Auth.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Signup from "./Signup";
+import Login from "./Login";
+
+const Auth = (props: any) => {
+  const [login, setLogin] = React.useState<boolean>(true);
+
+  function toggle(event: any) {
+    event.preventDefault();
+    setLogin(!login);
+    console.log("toggle -> login", login);
+  }
+
+  function toggler() {
+    return login ? (
+      <Signup updateToken={props.updateToken} />
+    ) : (
+      <Login updateToken={props.updateToken} />
+    );
+  }
+
+  return (
+    <div>
+      <h1>Sign up for me then</h1>
+      {toggler()}
+    </div>
+  );
+};
+
+export default Auth;
